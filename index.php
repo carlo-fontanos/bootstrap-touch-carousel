2360093f7d82d84656b31b0b013e8f0b7e5e4bce
Carl Victor Fontanos <carl.fontanos@gmail.com>
2017-12-28 11:12:09 +0800

    Moved files to github

---
+++ index.php
@@ -0,0 +1,253 @@
+<html lang="en">
+<head>
+	<title>TuuchCarousel Example</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="touchcarousel.css">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="touchcarousel.min.js"></script>
+</head>
+<body class="override">
+
+<div class="container">
+	<h3>Bootstrap Panels</h3>
+	<div id="bootstrap-categories" class="touchcarousel grey-blue">
+		<ul class="touchcarousel-container">
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (1).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (2).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (3).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (4).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (5).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>				
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (6).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (7).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (8).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (9).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>				
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (10).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>		
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (11).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>				
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (12).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (13).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (14).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (15).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (16).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (17).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (18).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (19).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (20).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (21).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (22).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (23).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+			<li class="touchcarousel-item">
+				<a class="item-block" href="javascript:;">
+					<div class="panel panel-default">
+						<div class="panel-heading">Panel Heading</div>
+						<img src="img/categories/1 (24).jpg" />
+						<div class="panel-footer">Panel Content</div>
+					</div>
+				</a>
+			</li>
+		</ul> 	
+	</div>
+</div>
+		
+<script type="text/javascript">
+jQuery(function($) {
+	carouselInstance = $("#bootstrap-categories").touchCarousel({
+		pagingNav: false,
+		snapToItems: false,
+		itemsPerMove: 5,
+		scrollToLast: false,
+		loopItems: false,
+		scrollbar: true,
+    }).data('touchCarousel');
+});
+</script>
+
+</body>
+</html>
